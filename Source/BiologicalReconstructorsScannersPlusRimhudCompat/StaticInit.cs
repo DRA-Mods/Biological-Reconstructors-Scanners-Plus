4a507e06bde51a442f60838bb9acba0c2ebc051a
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2023-02-28 02:16:32 +0100

    Updated RimHUD compat

--- Source/BiologicalReconstructorsScannersPlusRimhudCompat/StaticInit.cs
+++ Source/BiologicalReconstructorsScannersPlusRimhudCompat/StaticInit.cs
@@ -8,48 +8,43 @@ using RimHUD.Interface.Dialog;
 using RimHUD.Interface.HUD;
 using Verse;
 
-namespace BiologicalReconstructorsScannersPlus.Rimhud
+namespace BiologicalReconstructorsScannersPlus.Rimhud;
+
+public static class StaticInit
 {
-    public static class StaticInit
+    private const string ID = "AlphaMemesIsScanned";
+
+    public static void Init()
     {
-        private const string ID = "AlphaMemesIsScanned";
+        HudModel.Widgets.Add(ID, Value);
 
-        public static void Init(Harmony harmony)
+        BiologicalReconstructorsScannersPlusMod.Harmony.Patch(AccessTools.Method(typeof(GUIExtensions), nameof(GUIExtensions.ShowMenu)),
+            prefix: new HarmonyMethod(typeof(StaticInit), nameof(PreShowMenu)));
+    }
+
+    private static HudWidgetBase Value(PawnModel model)
+    {
+        var inDatabase = GetActiveAnimalDatabase.IsInDatabase(model.Base);
+        if (inDatabase == null) return null;
+
+        return HudValue.FromTextModel(new TextModel((inDatabase.Value ? "AM_Analyzed" : "AM_NotAnalyzed").Translate(), null, () =>
         {
-            HudModel.Widgets.Add(ID, model =>
-            {
-                var inDatabase = GetActiveAnimalDatabase.IsInDatabase(model.Base);
-                if (inDatabase == null)
-                    return null;
+            var database = GetActiveAnimalDatabase.GetDatabase();
+            if (database is { Spawned: true, Destroyed: false }) database.ShowAnimals();
+        }), Theme.RegularTextStyle);
+    }
 
-                return HudValue.FromText(
-                    (inDatabase.Value ? "AM_Analyzed" : "AM_NotAnalyzed").Translate(),
-                    null,
-                    Theme.RegularTextStyle,
-                    () =>
-                    {
-                        var database = GetActiveAnimalDatabase.GetDatabase();
-                        if (database is { Spawned: true, Destroyed: false })
-                            database.ShowAnimals();
-                    });
-            });
+    private static void PreShowMenu(ref IEnumerable<FloatMenuOption> self)
+    {
+        var floatMenuOptions = self as FloatMenuOption[] ?? self.ToArray();
+        if (floatMenuOptions.All(x => x.Label != HudModel.ElementComponents[0].Label)) return;
 
-            harmony.Patch(AccessTools.Method(typeof(GUIExtensions), nameof(GUIExtensions.ShowMenu)),
-                prefix: new HarmonyMethod(typeof(StaticInit), nameof(PreShowMenu)));
-        }
-
-        private static void PreShowMenu(ref IEnumerable<FloatMenuOption> self)
+        var custom = new FloatMenuOption("RimHUD.Model.Component.AlphaMemesIsScanned".Translate(), () =>
         {
-            var floatMenuOptions = self as FloatMenuOption[] ?? self.ToArray();
-            if (floatMenuOptions.All(x => x.Label != HudModel.ElementComponents[0].Label)) return;
-            
-            var custom = new FloatMenuOption("RimHUD.Model.Component.AlphaMemesIsScanned".Translate(), () =>
-            {
-                var tab = Find.WindowStack.WindowOfType<Dialog_Config>()._tabs._selected as Tab_ConfigContent;
-                tab?._editor.Add(new LayoutItem(LayoutItemType.Element, ID));
-            });
+            var tab = Find.WindowStack.WindowOfType<Dialog_Config>()._tabs._selected as Tab_ConfigContent;
+            tab?._editor.Add(new LayoutItem(LayoutItemType.Element, ID));
+        });
 
-            self = new List<FloatMenuOption>(floatMenuOptions).Concat(custom);
-        }
+        self = floatMenuOptions.Concat(custom);
     }
 }
