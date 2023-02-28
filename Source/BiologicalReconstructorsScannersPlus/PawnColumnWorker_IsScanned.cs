0f891e6030ff841cbf59aa6a40b18ee4918bc7ef
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2023-02-28 02:04:53 +0100

    Improvements to PawnColumnWorker
    Added tooltip to header, made it only visible if a database is active (the building exists)

--- Source/BiologicalReconstructorsScannersPlus/PawnColumnWorker_IsScanned.cs
+++ Source/BiologicalReconstructorsScannersPlus/PawnColumnWorker_IsScanned.cs
@@ -2,47 +2,48 @@
 using UnityEngine;
 using Verse;
 
-namespace BiologicalReconstructorsScannersPlus
+namespace BiologicalReconstructorsScannersPlus;
+
+[StaticConstructorOnStartup]
+public class PawnColumnWorker_IsScanned : PawnColumnWorker
 {
-    [StaticConstructorOnStartup]
-    public class PawnColumnWorker_IsScanned : PawnColumnWorker
+    private static Texture2D animalAnalysisTexture = ContentFinder<Texture2D>.Get("UI/Issues/AM_AnimalAnalysis");
+
+    public override bool VisibleCurrently => GetActiveAnimalDatabase.GetDatabase() != null;
+
+    public override void DoCell(Rect rect, Pawn pawn, PawnTable table)
     {
-        private static Texture2D animalAnalysisTexture = ContentFinder<Texture2D>.Get("UI/Issues/AM_AnimalAnalysis");
+        var isInDatabase = GetActiveAnimalDatabase.IsInDatabase(pawn);
 
-        public override void DoCell(Rect rect, Pawn pawn, PawnTable table)
-        {
-            var isInDatabase = GetActiveAnimalDatabase.IsInDatabase(pawn);
-
-            if (isInDatabase == null)
-                return;
+        if (isInDatabase == null)
+            return;
             
-            var checkboxRect = new Rect(rect.x + (int)((rect.width - 24f) / 2f), rect.y + Mathf.Max(3, 0), 24f, 24f);
+        var checkboxRect = new Rect(rect.x + (int)((rect.width - 24f) / 2f), rect.y + Mathf.Max(3, 0), 24f, 24f);
 
-            var color = GUI.color;
+        var color = GUI.color;
 
-            GUI.color = isInDatabase.Value ? Color.green : Color.red;
-            GUI.DrawTexture(checkboxRect, animalAnalysisTexture);
+        GUI.color = isInDatabase.Value ? Color.green : Color.red;
+        GUI.DrawTexture(checkboxRect, animalAnalysisTexture);
 
-            GUI.color = color;
+        GUI.color = color;
 
-            if (Mouse.IsOver(checkboxRect))
-            {
-                var tip = GetTip(isInDatabase);
-                if (!tip.NullOrEmpty())
-                    TooltipHandler.TipRegion(checkboxRect, tip);
-            }
-		}
-
-        public override void HeaderClicked(Rect headerRect, PawnTable table) 
-            => CameraJumper.TryJumpAndSelect(GetActiveAnimalDatabase.GetDatabase());
-
-        public string GetTip(bool? isInDatabase)
+        if (Mouse.IsOver(checkboxRect))
         {
-            if (isInDatabase == null) 
-                return null;
-            return (isInDatabase.Value ? "AM_Analyzed" : "AM_NotAnalyzed").Translate();
+            var tip = GetTip(isInDatabase);
+            if (!tip.NullOrEmpty())
+                TooltipHandler.TipRegion(checkboxRect, tip);
         }
-
-        public override string GetHeaderTip(PawnTable table) => null;
     }
+
+    public override void HeaderClicked(Rect headerRect, PawnTable table) 
+        => CameraJumper.TryJumpAndSelect(GetActiveAnimalDatabase.GetDatabase());
+
+    public static string GetTip(bool? isInDatabase)
+    {
+        if (isInDatabase == null) 
+            return null;
+        return (isInDatabase.Value ? "AM_Analyzed" : "AM_NotAnalyzed").Translate();
+    }
+
+    public override string GetHeaderTip(PawnTable table) => "BiologicalReconstructors.ScannersPlus.AlphaMemes.JumpToDatabase".Translate();
 }
